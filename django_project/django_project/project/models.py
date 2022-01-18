693c330ad63281a1312bdf81b5f7d9e9c37b7c2d
Vladimir Simin <vladimirsimin@Vladimirs-MacBook-Pro.local>
2022-01-18 13:51:11 +0100

    model and migration

--- django_project/django_project/project/models.py
+++ django_project/django_project/project/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class ToDo(models.Model):
+    PRIORITY_CHOICES = [
+        ('L', 'Low'),
+        ('M', 'Medium'),
+        ('H', 'High'),
+        ('C', 'Cricital')
+    ]
+    title = models.CharField(max_length=30)
+    priority = models.CharField(max_length=1, choices=PRIORITY_CHOICES)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
